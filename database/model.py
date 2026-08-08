5d8959e3f05b043fd24cda5dac639b8bc004f2e0
k1berteftel <kkulis985@gmail.com>
2026-08-08 20:03:12 +0300

    password added

--- database/model.py
+++ database/model.py
@@ -21,7 +21,7 @@ class UsersTable(Base):
     activity: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False), default=func.now())
     entry: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False), default=func.now())
 
-    authorized: Mapped[bool] = mapped_column(Boolean, default=False, server_default=None)
+    authorized: Mapped[bool] = mapped_column(Boolean, default=False, server_default=None, nullable=True)
 
 
 class AccountsTable(Base):
